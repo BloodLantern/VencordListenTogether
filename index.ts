97c92abd5b0cca4d8e0ce7a2a12ca3909be7733f
BloodLantern <maxime.gri.berlioz@gmail.com>
2025-02-09 14:44:35 +0100

    Fix CORS error

--- index.ts
+++ index.ts
@@ -4,25 +4,56 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { Logger } from "@utils/Logger";
 import definePlugin from "@utils/types";
+import { RestAPI } from "@webpack/common";
 
-import { MusicBeeRequest } from "./native";
-import { logger } from "./utils";
+export interface ActivityAssets {
+    large_image?: string;
+    large_text?: string;
+    small_image?: string;
+    small_text?: string;
+}
 
-let updated: boolean = false;
+export const enum ActivityType {
+    PLAYING = 0,
+    STREAMING = 1,
+    LISTENING = 2,
+    WATCHING = 3,
+    COMPETING = 5
+}
+
+export interface Activity {
+    state?: string;
+    details?: string;
+    timestamps?: {
+        start?: number;
+        end?: number;
+    };
+    assets?: ActivityAssets;
+    buttons?: Array<string>;
+    name: string;
+    application_id: string;
+    metadata?: {
+        button_urls?: Array<string>;
+    };
+    type: ActivityType;
+    url?: string;
+    flags: number;
+}
+
+export const logger = new Logger("MusicBeeListenTogether");
+
+export const musicBeePort = 9696;
+export const musicBeeBaseUrl = `http://localhost:${musicBeePort}/musicbee/`;
 
 export default definePlugin({
-    name: "MusicBee Together",
+    name: "MusicBeeListenTogether",
     description: "Adds a Listen Together button on the MusicBee activity",
     authors: [{ name: "BloodLantern", id: 256003154045435906n }, { name: "YohannDR", id: 418445860028940289n }],
 
     flux: {
         LOCAL_ACTIVITY_UPDATE(param: any | null | undefined) {
-            if (updated) {
-                updated = false;
-                return;
-            }
-
             if (param === null || param === undefined)
                 return;
             if (param.activity === null || param.activity === undefined)
@@ -30,19 +61,23 @@ export default definePlugin({
 
             const { activity } = param;
 
-            logger.info("Received local activity update for " + activity.name);
-
             if (activity.name !== "MusicBee")
                 return;
 
-            logger.info("Activity comes from MusicBee, requesting current track");
+            logger.info("Received local activity update for MusicBee, requesting current track");
 
-            activity.buttons = [
-                "Button1",
-                "Button2"
-            ];
-
-            MusicBeeRequest(activity);
+            handleRequest();
         },
     }
 });
+
+async function handleRequest() {
+    await RestAPI.get({ url: musicBeeBaseUrl + "currenttrack" })
+        .then(function (res) {
+            logger.info("Received response: " + res);
+        })
+        .catch(function (error) {
+            logger.error(error);
+        });
+}
+
